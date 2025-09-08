5d2ac284f5cb6783b8c4317645f51bf2f0781060
Melina Theill <meli.theill@gmail.com>
2025-09-08 19:51:05 -0300

    producto sql y entity producto

--- src/main/java/DAO/MySQL/ProductoMySQL.java
+++ src/main/java/DAO/MySQL/ProductoMySQL.java
@@ -2,9 +2,13 @@ package DAO.MySQL;
 
 import DAO.ProductoDAO;
 import entities.Producto;
+import entities.Producto_Factura;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
 
 public class ProductoMySQL implements ProductoDAO {
     private Connection conn;
@@ -19,22 +23,62 @@ public class ProductoMySQL implements ProductoDAO {
     }
 
     @Override
-    public void insert(Producto entity) throws SQLException {
-
+    public void insert(Producto producto) throws SQLException {
+        String insert = "INSERT INTO Producto (id_producto,nombre, valor) VALUES (?, ?, ?)";
+        PreparedStatement ps = this.conn.prepareStatement(insert);
+        ps.setInt(1, producto.getIdProducto());
+        ps.setString(2, producto.getNombre());
+        ps.setFloat(3, producto.getValor());
+        ps.executeUpdate();
+        ps.close();
+        conn.commit();
     }
 
     @Override
-    public boolean delete(int id) throws SQLException {
-        return false;
+    public boolean delete(int id_producto) throws SQLException {
+        if(find(id_producto)==null){
+            return false;
+        }
+        String delete = "DELETE FROM Producto WHERE id_producto = ?";
+        PreparedStatement ps = this.conn.prepareStatement(delete);
+        ps.setInt(1, id_producto);
+        ps.executeUpdate();
+        ps.close();
+        conn.commit();
+        return true;
     }
 
     @Override
-    public boolean update(Producto entity) throws SQLException {
-        return false;
+    public boolean update(Producto producto) throws SQLException {
+        if (find(producto.getIdProducto()) == null){
+            return false;
+        }
+        String update = "UPDATE Producto SET nombre = ?, valor = ? WHERE id_producto = ?";
+        PreparedStatement ps = this.conn.prepareStatement(update);
+        ps.setInt(1, producto.getIdProducto());
+        ps.setString(2, producto.getNombre());
+        ps.setFloat(3, producto.getValor());
+        ps.executeUpdate();
+        ps.close();
+        conn.commit();
+        return true;
     }
 
     @Override
-    public Producto find(int id) throws SQLException {
-        return null;
+    public Producto find(int id_producto) throws SQLException {
+        Producto pd  = null;
+        String select = "SELECT * FROM Producto WHERE id_producto = ?";
+        PreparedStatement ps = this.conn.prepareStatement(select);
+        ps.setInt(1, id_producto);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            String nombre = rs.getString(2);
+            Float valor = rs.getFloat(3);
+
+            pd = new Producto (id_producto, nombre, valor);
+        }
+
+        return pd;
     }
+
 }
