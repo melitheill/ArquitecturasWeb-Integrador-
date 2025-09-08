240b8316d635be23c933a2da62e02fe546f16ff3
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2025-09-08 18:41:44 -0300

    algo de Producto

--- src/main/java/DAO/MySQL/ProductoMySQL.java
+++ src/main/java/DAO/MySQL/ProductoMySQL.java
@@ -1,13 +1,40 @@
 package DAO.MySQL;
 
 import DAO.ProductoDAO;
+import entities.Producto;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ProductoMySQL implements ProductoDAO {
     private Connection conn;
 
     public ProductoMySQL(Connection conn) {
         this.conn = conn;
+    }
+
+    @Override
+    public Producto getProductoMayorFacturacion() {
+        return null;
+    }
+
+    @Override
+    public void insert(Producto entity) throws SQLException {
+
+    }
+
+    @Override
+    public boolean delete(int id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Producto entity) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Producto find(int id) throws SQLException {
+        return null;
     }
 }
