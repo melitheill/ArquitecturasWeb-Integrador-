f110f82c76bc6950b1a880b6ab7a7b971d17d29a
Tomas <tgomez@alumnos.exa.unicen.edu.ar>
2025-10-15 15:52:39 -0300

    inicializa integrador 3

---
+++ integrador-3/src/test/java/org/grupo14/integrador3/Integrador3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupo14.integrador3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Integrador3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
