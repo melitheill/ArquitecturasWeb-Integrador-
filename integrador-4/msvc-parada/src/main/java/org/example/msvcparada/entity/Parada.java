0a3d5698dff44ced508edc87b5dbb80a34799368
Tomas <tgomez@alumnos.exa.unicen.edu.ar>
2025-11-12 03:40:12 -0300

    acceso a todos los microservicios mediante el gateway

--- integrador-4/msvc-parada/src/main/java/org/example/msvcparada/entity/Parada.java
+++ integrador-4/msvc-parada/src/main/java/org/example/msvcparada/entity/Parada.java
@@ -1,6 +1,20 @@
 package org.example.msvcparada.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Parada {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String direccion;
     private String nombre;
